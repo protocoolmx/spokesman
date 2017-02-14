729151b76ec291dca0dfc7aac5b6066b20ad78e4
Joshua <joshua.marquezn@gmail.com>
2017-02-14 15:58:06 -0700

    Example code added

---
+++ example/MyEmitter.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const ServiceEmitter = require('../index').ServiceEmitter;
+
+let singleton = Symbol();
+let singletonEnforcer = Symbol();
+
+class MyEmitter extends ServiceEmitter {
+
+  constructor (enforcer) {
+
+    // This disables creating instance outside.
+    if (enforcer !== singletonEnforcer) {
+      throw 'Cannot construct singleton';
+    }
+
+    super({ delay: 500 }); // Call `ServiceEmitter` constructor.
+  }
+
+  /**
+   * Get unique instance of `MyEmitter`.
+   */
+  static getInstance () {
+    if (!this[singleton]) {
+      this[singleton] = new MyEmitter(singletonEnforcer);
+    }
+
+    return this[singleton];
+  }
+
+  /**
+   * @override
+   */
+  onProviderData (data) {
+    // Emit data received from provider to custom emitters.
+    //
+    // Note: if you call `super({ autoRequest: false })`, then this method will
+    // not get fired unless you call first `this.provider.requestData(opts)` in
+    // the override of `interval()`.
+    this.emit('data', data);
+
+    return true; // This means `data` received is valid.
+  }
+
+  /**
+   * @override
+   */
+  onProviderError (err) {
+    // Emit error received from provider to custom emitters.
+    this.emit('error', err);
+  }
+}
+
+module.exports = MyEmitter;
